0e082bc16ebc830201dd308cf25d76ac0d4050f0
StevenDong2512 <stevendong1997@outlook.com>
2023-04-22 15:06:01 +1000

    test

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generator = require('./lib/generate')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateLogo = require("./lib/generate");
 
 const questions = [
-    {
-        type: "list",
-        name: "shape",
-        message: "Please pick the shape for your logo.",
-        choices: ["Circle", "Square", "Triangle"],
-    },
-    {
-        type: "input",
-        name: "shape-color",
-        message: "Please enter a colour keyword or a hexadecimal number to fill your logo shape.",
-    },
-    {
-        type: "input",
-        name: "text",
-        message: "Please enter UP TO THREE letters for your logo text.",
-    },
-    {
-        type: "input",
-        name: "text-color",
-        message: "Please enter a colour keyword or a hexadecimal number for text colour.",
-    },];
+  {
+    type: "list",
+    name: "shape",
+    message: "Please pick the shape for your logo.",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message:
+      "Please enter a colour keyword or a hexadecimal number to fill your logo shape.",
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "Please enter UP TO THREE letters for your logo text.",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Please enter a colour keyword or a hexadecimal number for text colour.",
+  },
+];
 
 function writeToFile(fileName, data) {
-    const generated = generator(data);
-    fs.writeFile(fileName, generated, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Your logo has been generated.");
-    })
-};
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your logo has been generated.");
+  });
+}
 
 function init() {
-    inquirer.promote(questions)
-        .then(function (data) {
-            writeToFile(logo.svg, data);
-        })
-};
+  inquirer.prompt(questions).then(function (response) {
+    console.log(response);
+    writeToFile("logo.svg", generateLogo(response));
+  });
+}
 
-init()
+init();
